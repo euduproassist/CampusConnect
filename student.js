12177ad57e364f8b5c3f8f3077785a7aa3e8796f
eduproassist <eduproassist44@gmail.com>
2026-03-13 05:13:58 +0200

    Update student.js

--- student.js
+++ student.js
@@ -1,4 +1,9 @@
 import { auth, db, storage } from './firebase-config.js';
+import { addDoc, collection } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+import { ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-storage.js";
+
+
+import { auth, db, storage } from './firebase-config.js';
 import { addDoc, collection } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
