f2afe0665023db9029ac2bc04d79db020ef8b42b
eduproassist <eduproassist44@gmail.com>
2026-06-25 13:23:21 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-storage.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-analytics.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSqFUdUshBjfZzlYpQfzjVkfDySJKvcvI",
-  authDomain: "campusconnect-4bec5.firebaseapp.com",
-  projectId: "campusconnect-4bec5",
-  storageBucket: "campusconnect-4bec5.firebasestorage.app",
-  messagingSenderId: "164114642929",
-  appId: "1:164114642929:web:28ca6ef8fc1b99539af650",
-  measurementId: "G-M2GYWJ8M0N"
+  apiKey: "AIzaSyCNTq3n-NX7qg1lvmhu9pWbjLMqZVX_OJE",
+  authDomain: "qualify-sa.firebaseapp.com",
+  projectId: "qualify-sa",
+  storageBucket: "qualify-sa.firebasestorage.app",
+  messagingSenderId: "1056702711871",
+  appId: "1:1056702711871:web:b3284aaa05a53c696e908a",
+  measurementId: "G-KE8E5ZY403"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Services to be exported
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 
